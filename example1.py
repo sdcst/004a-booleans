cd186aece4a02f72ae3219a64d94c6c4b3e08791
Chris Yang <cyang@deltalearns.ca>
2020-09-25 14:01:55 -0700

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,16 @@
+#! python3
+# this file allows you to test how boolean variables and conditional statements work
+
+
+a = 10
+b = 5
+
+value = a==b
+
+
+print(value)
+
+
+if (a == b):
+  print("The two values are equal")
+  
