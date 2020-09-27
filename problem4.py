b0a11a351b0c26fb9a8d3a38e44a2a250a4cb92a
Chris Yang <cyang@deltalearns.ca>
2020-09-27 15:38:27 -0700

    Create problem4.py

---
+++ problem4.py
@@ -0,0 +1,14 @@
+#! python3
+# Have the user enter in 3 numerical values, representing the side lengths of a triangle. 
+# Determine if the values are close enough to make a right triangle. 
+# It is close enough if the expected length of the hypotenuse and the actual length 
+# differ by no more than 2% 
+# (2 marks)
+
+# Inputs:
+# - 3 numbers, in any order
+
+# Outputs:
+# - "that is a right triangle"
+# - "that is an acute triangle"
+# - "that is an obtuse triangle"
