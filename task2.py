eaf178e3964a555c2efdc49f5ee70bc81f578f20
Chris Yang <cyang@deltalearns.ca>
2020-09-27 15:13:56 -0700

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,12 @@
+#! python3
+# Have the user input a number 
+# Determine if the number is positive, negative or 0 
+# 2 points
+
+# Inputs:
+# number
+
+# Outputs:
+# - "positive"
+# - "negative"
+# - "zero"
