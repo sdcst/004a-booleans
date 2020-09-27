c14d4a9a45b1b1cfc69e2feb1c490a9c6b4d0b3b
Chris Yang <cyang@deltalearns.ca>
2020-09-27 15:23:30 -0700

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,11 @@
+#! python3
+
+# Have the user enter in a sentence. 
+# Check to see if the word "password" is located in the sentence
+
+# Inputs:
+# a sentence
+
+# Outputs:
+# "the sentence contains password"
+# "the sentence does not contain password"
