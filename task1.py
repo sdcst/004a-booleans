74c1c206d787fe0ea6a0855eb4a89c3f0695eb11
Chris Yang <cyang@deltalearns.ca>
2020-09-27 15:12:26 -0700

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,13 @@
+#! python3
+
+# Have the user input a number. 
+# Determine if the number is larger than 100 
+# If it is, the output should read "The number is larger than 100" 
+# (2 points)
+# Inputs:
+# number
+
+# Outputs:
+# "The number is larger than 100"
+# "The number is smaller than 100"
+# "The number is 100"
