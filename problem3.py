46286b7d763734cd394eefc0496ece7d18e22189
Chris Yang <cyang@deltalearns.ca>
2020-09-27 15:27:44 -0700

    Create problem3.py

---
+++ problem3.py
@@ -0,0 +1,8 @@
+#! python3
+
+#  Have the user enter a username 
+# If the username is not "admin" then tell them it is an "invalid user", 
+# but if it is, then ask them for a password 
+# If they get the password correct (password is 12345password) 
+# then display the message "Access granted"
+# 1 marks
